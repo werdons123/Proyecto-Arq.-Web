cd9aa92f29a93a9e9a5e13b821a5060fdf860beb
werdons123 <94635164+werdons123@users.noreply.github.com>
2024-11-17 20:28:43 -0500

    Delete Proyecto directory

--- Proyecto/src/app/models/Contacto_ayuda.ts
+++
@@ -1,8 +0,0 @@
-
-export class Contacto_ayuda{
-    idContactoAyuda:number=0
-    nombreInstitucion:string=""
-    descripcion:string=""
-    telefono:string=""
-
-}
--- Proyecto/src/app/models/Zona.ts
+++
@@ -1,9 +0,0 @@
-export class Zona{
-    id_Zona:number=0
-    nombre_zona:string=""
-    descripcion:string=""
-    ubicacion:string=""
-    latitud:number=0
-    longitud:number=0
-
-}
