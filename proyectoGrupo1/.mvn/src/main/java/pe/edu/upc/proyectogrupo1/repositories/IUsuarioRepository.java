da6e8d171b4392af8699aab0272d3fa542474119
werdons123 <94635164+werdons123@users.noreply.github.com>
2024-11-18 19:29:25 -0500

    Delete proyectoGrupo1 directory

--- proyectoGrupo1/.mvn/src/main/java/pe/edu/upc/proyectogrupo1/repositories/IUsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.proyectogrupo1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.proyectogrupo1.entities.Usuario;
-
-public interface IUsuarioRepository extends JpaRepository<Usuario,Integer> {
-
-}
--- proyectoGrupo1/src/main/java/pe/edu/upc/proyectogrupo1/repositories/IZonaAlertaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.proyectogrupo1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.proyectogrupo1.entities.ZonaAlerta;
-@Repository
-
-public interface IZonaAlertaRepository extends JpaRepository<ZonaAlerta,Integer> {
-}
